fb98f32266b3cb6016669d21f6b72b96724fe8de
Shodhan11 <59709509+Shodhan11@users.noreply.github.com>
2021-10-03 21:18:02 -0400

    Update redirections.c
    Added some comments for students TODO

--- redirections/redirections.c
+++ redirections/redirections.c
@@ -7,6 +7,7 @@
 int main (void) {
   // Part 1: Read from a file and print to the terminal -----
   // Open a read-only file descriptor  
+  // TODO 1: What is the meaning of O_RDONLY, and why is this flag used here?
   int input_df = open("input_file.txt", O_RDONLY);
   char buf[1024];
   int len = read(input_df, buf, 1024);
@@ -15,7 +16,7 @@ int main (void) {
   
   // Part 2: Redirect STDOUT to a new file and write into the file -----
   // Open another file descriptor for writing.
-  // TODO 1: What are the meanings of O_WRONLY, O_CREAT, O_TRUNC?
+  // TODO 1: What are the meanings of O_WRONLY, O_CREAT, O_TRUNC, and why are these flags used here?
   int new_stdout = open("new_stdout", O_WRONLY | O_CREAT, 0644);
   dup2(new_stdout, STDOUT_FILENO);
   write(STDOUT_FILENO, "Part 2: Hello World!!!", 14);
@@ -23,7 +24,7 @@ int main (void) {
   exit (0);
   
   // TODO 2: Compile and run the program
-  // gcc redirections.c -o redicteions
+  // gcc redirections.c -o redirections
   // To Learn more
   // https://www.geeksforgeeks.org/input-output-system-calls-c-create-open-close-read-write/
 }
